af8f935194dfb1e90609678407710c58eaca5c39
vraiSlophil <88555261+vraiSlophil@users.noreply.github.com>
2022-12-21 15:05:40 +0100

    database update

--- app/database.php
+++ app/database.php
@@ -1,8 +1,14 @@
 <?php
 
 class Database {
-    function sql_connect(){
+
+    private function sql_connect() {
         include "database_info.php";
+
+        global $database;
+        global $port;
+        global $username;
+        global $password;
 
         try {
             $sch='mysql:host=localhost;dbname='.$database.';port='.$port;
@@ -12,6 +18,58 @@ class Database {
             die('Erreur : '.$e->getMessage());
         }
         return $bdd;
+    }
+
+    public function createUser($username, $password, $email, $firstName = null, $lastName = null, $profilePictureUrl = null): bool|string {
+        $salt = generateUuid();
+        $hashed_password = hash('sha256', $password . $salt);
+        $query = "INSERT INTO users (username, password, salt, email, first_name, last_name, profile_picture_url) VALUES (:username, :password, :salt, :email, :first_name, :last_name, :profile_picture_url)";
+        $pdo = $this->sql_connect();
+        $stmt = $pdo->prepare($query);
+        $stmt->bindValue(':username', $username);
+        $stmt->bindValue(':password', $hashed_password);
+        $stmt->bindValue(':salt', $salt);
+        $stmt->bindValue(':email', $email);
+        $stmt->bindValue(':first_name', $firstName);
+        $stmt->bindValue(':last_name', $lastName);
+        $stmt->bindValue(':profile_picture_url', $profilePictureUrl);
+
+        // Exécuter la requête
+        try {
+            $stmt->execute();
+            return true;
+        } catch (PDOException $e) {
+            if ($e->errorInfo[1] == 1062) {
+                // Code d'erreur MySQL 1062: erreur de clé unique
+                return "Un compte avec ce nom d'utilisateur ou cet email existe déjà.";
+            } else if ($e->errorInfo[1] == 1452) {
+                // Code d'erreur MySQL 1452: erreur de clé étrangère
+                return "Adresse email non valide.";
+            } else if ($e->errorInfo[1] == 1406) {
+                return "Votre nom d'utilisateur, votre email, ou votre mot de passe est trop long.";
+            } else {
+                return "Une erreur inconnue s'est produite lors de la création du compte. Veuillez contacter un administrateur.";
+            }
+        }
+    }
+
+    public function createTheme($theme_name, $theme_color): bool|string {
+        $pdo = $this->sql_connect();
+
+        $stmt = $pdo->prepare("INSERT INTO themes (theme_name, theme_color) VALUES (:theme_name, :theme_color)");
+        $stmt->bindParam(":theme_name", $theme_name);
+        $stmt->bindParam(":theme_color", $theme_color);
+
+        try {
+            $stmt->execute();
+            return true;
+        } catch (PDOException $e) {
+             if ($e->errorInfo[1] == 1452) {
+                return "La couleur spécifiée n'est pas valide.";
+            } else {
+                return "Une erreur inconnue s'est produite lors de la création du compte. Veuillez contacter un administrateur.";
+            }
+        }
     }
 
 }    
--- app/database_structure/FED.sql
+++ app/database_structure/FED.sql
@@ -1,41 +1,173 @@
-CREATE TABLE users (
-   user_id INTEGER NOT NULL AUTO_INCREMENT,
-   username TEXT NOT NULL UNIQUE,
-   password TEXT NOT NULL,
-   salt VARCHAR(255) NOT NULL UNIQUE,
-   email TEXT NOT NULL UNIQUE,
-   first_name TEXT,
-   last_name TEXT,
-   birth_date DATE,
-   profile_picture_url TEXT,
-   CHECK (`email` RLIKE '[a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+\\.[a-zA-Z]{2,}'),
-   PRIMARY KEY (user_id)
-);
+-- phpMyAdmin SQL Dump
+-- version 5.1.3
+-- https://www.phpmyadmin.net/
+--
+-- Hôte : localhost:3306
+-- Généré le : mer. 21 déc. 2022 à 14:01
+-- Version du serveur : 10.6.11-MariaDB-0ubuntu0.22.04.1
+-- Version de PHP : 7.4.32
 
-CREATE TABLE themes (
-    theme_id INTEGER NOT NULL AUTO_INCREMENT,
-    theme_name TEXT NOT NULL UNIQUE,
-    theme_color TEXT NOT NULL DEFAULT '#FAEAB1',
-    PRIMARY KEY (theme_id)
-);
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+START TRANSACTION;
+SET time_zone = "+00:00";
 
-CREATE TABLE tasks (
-   task_id INTEGER NOT NULL AUTO_INCREMENT,
-   title TEXT NOT NULL,
-   description TEXT,
-   user_id INTEGER NOT NULL,
-   theme_id INTEGER NOT NULL,
-   task_status BOOLEAN NOT NULL DEFAULT FALSE,
-   PRIMARY KEY (task_id),
-   FOREIGN KEY (user_id) REFERENCES users(user_id),
-   FOREIGN KEY (theme_id) REFERENCES themes(theme_id)
-);
 
-CREATE TABLE authorized_themes (
-   authorized_theme_id INTEGER NOT NULL AUTO_INCREMENT,
-   theme_id INTEGER NOT NULL,
-   user_id INTEGER NOT NULL,
-   PRIMARY KEY (authorized_theme_id),
-   FOREIGN KEY (theme_id) REFERENCES themes(theme_id),
-   FOREIGN KEY (user_id) REFERENCES users(user_id)
-);
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8mb4 */;
+
+--
+-- Base de données : `FED`
+--
+
+-- --------------------------------------------------------
+
+--
+-- Structure de la table `authorized_themes`
+--
+
+CREATE TABLE `authorized_themes` (
+                                     `authorized_theme_id` int(11) NOT NULL,
+                                     `theme_id` int(11) NOT NULL,
+                                     `user_id` int(11) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb3 COLLATE=utf8mb3_general_ci;
+
+-- --------------------------------------------------------
+
+--
+-- Structure de la table `tasks`
+--
+
+CREATE TABLE `tasks` (
+                         `task_id` int(11) NOT NULL,
+                         `title` varchar(255) NOT NULL,
+                         `description` text DEFAULT NULL,
+                         `user_id` int(11) NOT NULL,
+                         `theme_id` int(11) NOT NULL,
+                         `task_status` tinyint(1) NOT NULL DEFAULT 0
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb3 COLLATE=utf8mb3_general_ci;
+
+-- --------------------------------------------------------
+
+--
+-- Structure de la table `themes`
+--
+
+CREATE TABLE `themes` (
+                          `theme_id` int(11) NOT NULL,
+                          `author_id` int(11) DEFAULT NULL,
+                          `theme_name` varchar(255) NOT NULL,
+                          `theme_color` char(7) NOT NULL DEFAULT '#FAEAB1' CHECK (`theme_color` regexp '^#[a-fA-F0-9]{6}$')
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb3 COLLATE=utf8mb3_general_ci;
+
+-- --------------------------------------------------------
+
+--
+-- Structure de la table `users`
+--
+
+CREATE TABLE `users` (
+                         `user_id` int(11) NOT NULL,
+                         `username` varchar(24) NOT NULL,
+                         `password` varchar(64) NOT NULL,
+                         `salt` varchar(255) NOT NULL,
+                         `email` varchar(64) NOT NULL,
+                         `first_name` varchar(32) DEFAULT NULL,
+                         `last_name` varchar(32) DEFAULT NULL,
+                         `profile_picture_url` text DEFAULT NULL
+) ;
+
+--
+-- Index pour les tables déchargées
+--
+
+--
+-- Index pour la table `authorized_themes`
+--
+ALTER TABLE `authorized_themes`
+    ADD PRIMARY KEY (`authorized_theme_id`),
+    ADD KEY `theme_id` (`theme_id`),
+    ADD KEY `user_id` (`user_id`);
+
+--
+-- Index pour la table `tasks`
+--
+ALTER TABLE `tasks`
+    ADD PRIMARY KEY (`task_id`),
+    ADD KEY `user_id` (`user_id`),
+    ADD KEY `theme_id` (`theme_id`);
+
+--
+-- Index pour la table `themes`
+--
+ALTER TABLE `themes`
+    ADD PRIMARY KEY (`theme_id`),
+    ADD UNIQUE KEY `theme_name` (`theme_name`),
+    ADD KEY `author_id` (`author_id`);
+
+--
+-- Index pour la table `users`
+--
+ALTER TABLE `users`
+    ADD PRIMARY KEY (`user_id`),
+    ADD UNIQUE KEY `username` (`username`),
+    ADD UNIQUE KEY `salt` (`salt`),
+    ADD UNIQUE KEY `email` (`email`);
+
+--
+-- AUTO_INCREMENT pour les tables déchargées
+--
+
+--
+-- AUTO_INCREMENT pour la table `authorized_themes`
+--
+ALTER TABLE `authorized_themes`
+    MODIFY `authorized_theme_id` int(11) NOT NULL AUTO_INCREMENT;
+
+--
+-- AUTO_INCREMENT pour la table `tasks`
+--
+ALTER TABLE `tasks`
+    MODIFY `task_id` int(11) NOT NULL AUTO_INCREMENT;
+
+--
+-- AUTO_INCREMENT pour la table `themes`
+--
+ALTER TABLE `themes`
+    MODIFY `theme_id` int(11) NOT NULL AUTO_INCREMENT;
+
+--
+-- AUTO_INCREMENT pour la table `users`
+--
+ALTER TABLE `users`
+    MODIFY `user_id` int(11) NOT NULL AUTO_INCREMENT;
+
+--
+-- Contraintes pour les tables déchargées
+--
+
+--
+-- Contraintes pour la table `authorized_themes`
+--
+ALTER TABLE `authorized_themes`
+    ADD CONSTRAINT `authorized_themes_ibfk_1` FOREIGN KEY (`theme_id`) REFERENCES `themes` (`theme_id`),
+    ADD CONSTRAINT `authorized_themes_ibfk_2` FOREIGN KEY (`user_id`) REFERENCES `users` (`user_id`);
+
+--
+-- Contraintes pour la table `tasks`
+--
+ALTER TABLE `tasks`
+    ADD CONSTRAINT `tasks_ibfk_1` FOREIGN KEY (`user_id`) REFERENCES `users` (`user_id`),
+    ADD CONSTRAINT `tasks_ibfk_2` FOREIGN KEY (`theme_id`) REFERENCES `themes` (`theme_id`);
+
+--
+-- Contraintes pour la table `themes`
+--
+ALTER TABLE `themes`
+    ADD CONSTRAINT `themes_ibfk_1` FOREIGN KEY (`author_id`) REFERENCES `users` (`user_id`) ON DELETE CASCADE;
+COMMIT;
+
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
